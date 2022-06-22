44699f6702b05238f74dc4b22148bd86ac4e56d9
LeandroFilie <leandro.gfilie@gmail.com>
2022-06-22 15:30:31 -0300

    feat: add event page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import Event from "./pages/Event"
+
 function App() {
   return (
-    <h1>Hello world</h1>
+    <Event />
   )
 }
 
---
+++ src/pages/Event.tsx
@@ -0,0 +1,8 @@
+import Header from "../components/Header";
+
+export default function Event() {
+  return (
+    <Header />
+    
+  )
+}
